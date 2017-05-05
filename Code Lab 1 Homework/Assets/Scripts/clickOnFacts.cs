f6a5787ffd04d6e51eda1f6aeecee0b075397dd7
Mostafa Haque <93haque@gmail.com>
2017-05-05 15:29:03 -0400

    Clicking on object returns Cube

---
+++ Code Lab 1 Homework/Assets/Scripts/clickOnFacts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickOnFacts : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { //On mouse button down
+            RaycastHit rayHit; //create a RaycastHit object
+
+            //Call the Physics.Raycast function with Camera world position of the mouse, the rayHit object, and the far clipping plane
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Camera.main.farClipPlane))
+            {
+                Debug.Log(rayHit.collider.name); //Print the name of the object
+                if (rayHit.collider.tag == "Player1")
+                {
+                   
+                    Destroy(rayHit.collider.gameObject);
+                }
+                if (rayHit.collider.tag == "Player2")
+                {
+                    
+                    Destroy(rayHit.collider.gameObject);
+                }
+            }
+            else
+            {
+                Debug.Log("Hit nothing"); //if you hit nothing, print that
+            }
+        }
+    }
+}
